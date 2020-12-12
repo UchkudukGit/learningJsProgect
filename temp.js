93cdd3ecae8b25a639a56e9255fc73c40819c468
UckudukGit <nikonov.learning@gmail.com>
2020-12-12 10:37:01 +0300

    30_html-elements

--- temp.js
+++ temp.js
@@ -1,15 +1,3 @@
 "use strict";
 
-const obj = {
-    name: "vova",
-    credentions: {
-        login: "log",
-        pass: "12345"
-    }
-};
-
-const add = {
-    isTrue: true
-};
-
-console.log(Object.assign(obj, add.isTrue));
+console.log([] + 3);
